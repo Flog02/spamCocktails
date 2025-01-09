7efbe5ff9c916de5245dccf11a584e0b3df13e8f
Flog02 <flogertymeraj3@gmail.com>
2025-01-09 11:54:17 +0100

    http fixx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,22 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(){}
+  // cocktails: any[] = [];
+
+  // constructor(private http: HttpClient) {
+  //   this.loadCocktails();
+  // }
+
+  // loadCocktails(): void {
+  //   this.getCocktails().subscribe((response) => {
+  //     this.cocktails = response;
+  //   });
+  // }
+
+  // getCocktails(): Observable<any[]> {
+  //   const apiUrl = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'; // Replace with your API
+  //   return this.http.get<any[]>(apiUrl);
+  // }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,12 +3,16 @@ import { IonHeader, IonToolbar,IonRouterLink, IonContent,IonButtons,IonTitle,Ion
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';  // <-- Import FormsModule
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [FormsModule, IonHeader, IonToolbar, IonRouterLink, IonContent, FontAwesomeModule, IonButtons, IonTitle, IonMenuButton, IonMenu, IonMenuToggle, IonButton, IonItem, IonList, RouterLink],
+  imports: [HttpClientModule,NgFor,FormsModule, IonHeader, IonToolbar, IonRouterLink, IonContent, FontAwesomeModule, IonButtons, IonTitle, IonMenuButton, IonMenu, IonMenuToggle, IonButton, IonItem, IonList, RouterLink],
 })
 export class HomePage {
   searchQuery: string = '';
@@ -33,7 +37,12 @@ export class HomePage {
     },
     // Add other drinks...
   ];
-  constructor(private router: Router) {}
+  constructor(private router: Router,private http: HttpClient) {
+    this.loadCocktails();
+
+console.log(this.cocktails)
+
+  }
 
   get filteredDrinks() {
     return this.drinks.filter(drink =>
@@ -44,6 +53,21 @@ export class HomePage {
   onDrinkClick(drink: any) {
     this.router.navigate(['/drink-detail', drink.name]); // Navigate to the detail page with the drink's name
   }
+  cocktails: any[] = [];
+  
+    
+  
+    loadCocktails(): void {
+      this.getCocktails().subscribe((response:any) => {
+        this.cocktails = response;
+      });
+    }
+  
+    getCocktails(): Observable<any[]> {
+      const apiUrl = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'; // Replace with your API
+      return this.http.get<any[]>(apiUrl);
+    }
 
+    
 
 }
