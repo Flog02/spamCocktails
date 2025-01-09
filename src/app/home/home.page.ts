3ec7c9b4148265c73457c2df0e49f4ad286873f0
Flog02 <flogertymeraj3@gmail.com>
2025-01-09 12:11:40 +0100

    http

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar,IonRouterLink, IonContent,IonButtons,IonTitle,IonMenuButton,IonMenu,IonMenuToggle,IonButton,IonList,IonItem} from '@ionic/angular/standalone';
+import { IonCardTitle,IonHeader,IonCard,IonCardHeader,IonCardContent,IonGrid,IonRow,IonCol, IonToolbar,IonRouterLink, IonContent,IonButtons,IonTitle,IonMenuButton,IonMenu,IonMenuToggle,IonButton,IonList,IonItem} from '@ionic/angular/standalone';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';  // <-- Import FormsModule
@@ -12,62 +12,52 @@ import { HttpClientModule } from '@angular/common/http';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [HttpClientModule,NgFor,FormsModule, IonHeader, IonToolbar, IonRouterLink, IonContent, FontAwesomeModule, IonButtons, IonTitle, IonMenuButton, IonMenu, IonMenuToggle, IonButton, IonItem, IonList, RouterLink],
+  imports: [IonCardTitle,IonHeader,IonCard,IonCardHeader,IonCardContent,IonGrid,IonRow,IonCol,HttpClientModule,NgFor,FormsModule, IonToolbar, IonRouterLink, IonContent, FontAwesomeModule, IonButtons, IonTitle, IonMenuButton, IonMenu, IonMenuToggle, IonButton, IonItem, IonList, RouterLink],
 })
 export class HomePage {
-  searchQuery: string = '';
-  drinks = [
-    {
-      name: 'Mojito',
-      image: 'assets/images/mojito.jpg',
-      ingredients: 'Rum, Mint, Lime, Sugar, Soda',
-      preparation: 'Muddle mint and sugar, add rum and lime, top with soda.',
-    },
-    {
-      name: 'Martini',
-      image: 'assets/images/martini.jpg',
-      ingredients: 'Gin, Dry Vermouth',
-      preparation: 'Stir gin and vermouth, strain into a chilled glass.',
-    },
-    {
-      name: 'Margarita',
-      image: 'assets/images/margarita.jpg',
-      ingredients: 'Tequila, Lime, Triple sec, Salt',
-      preparation: 'Shake tequila, lime, and triple sec, strain into a salted rimmed glass.',
-    },
-    // Add other drinks...
-  ];
-  constructor(private router: Router,private http: HttpClient) {
-    this.loadCocktails();
+  
+  // drinks = [
+  //   {
+  //     name: 'Mojito',
+  //     image: 'assets/images/mojito.jpg',
+  //     ingredients: 'Rum, Mint, Lime, Sugar, Soda',
+  //     preparation: 'Muddle mint and sugar, add rum and lime, top with soda.',
+  //   },
+  //   {
+  //     name: 'Martini',
+  //     image: 'assets/images/martini.jpg',
+  //     ingredients: 'Gin, Dry Vermouth',
+  //     preparation: 'Stir gin and vermouth, strain into a chilled glass.',
+  //   },
+  //   {
+  //     name: 'Margarita',
+  //     image: 'assets/images/margarita.jpg',
+  //     ingredients: 'Tequila, Lime, Triple sec, Salt',
+  //     preparation: 'Shake tequila, lime, and triple sec, strain into a salted rimmed glass.',
+  //   },
+  //   // Add other drinks...
+  // ];
+  searchQuery: string = '';  // Search query for user input
+  cocktails: any[] = [];     // Cocktails array to hold API response
 
-console.log(this.cocktails)
-
+  constructor(private router: Router, private http: HttpClient) {
+    this.loadCocktails();  // Load cocktails on component initialization
+    console.log(this.cocktails);  // Debugging: Output cocktails to console
   }
 
-  get filteredDrinks() {
-    return this.drinks.filter(drink =>
-      drink.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-    );
+  loadCocktails(): void {
+    this.getCocktails().subscribe((response: any) => {
+      this.cocktails = response.drinks;  // Correct property access for cocktail API
+    });
+  }
+
+  getCocktails(): Observable<any> {  // Type as `Observable<any>` since the API response structure may vary
+    const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';  // Corrected to HTTPS
+    return this.http.get<any>(apiUrl);
   }
 
   onDrinkClick(drink: any) {
-    this.router.navigate(['/drink-detail', drink.name]); // Navigate to the detail page with the drink's name
+    this.router.navigate(['/drink-detail', drink.name]);  // Navigate to drink detail page
   }
-  cocktails: any[] = [];
-  
-    
-  
-    loadCocktails(): void {
-      this.getCocktails().subscribe((response:any) => {
-        this.cocktails = response;
-      });
-    }
-  
-    getCocktails(): Observable<any[]> {
-      const apiUrl = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'; // Replace with your API
-      return this.http.get<any[]>(apiUrl);
-    }
-
-    
 
 }
